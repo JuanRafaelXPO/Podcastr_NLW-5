7f000eccf1e28584bd7b3e87daa6d0593c82f98f
Juan Rafael <juanrafaelxpo@outlook.com>
2021-05-24 20:20:04 -0300

    Criação e estilização das páginas de episódios

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,10 +1,14 @@
 import { parseISO, format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router'
 
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
+import styles from './episode.module.scss';
 
 type Episode = {
     id: string;
@@ -24,7 +28,34 @@ type EpisodeProps = {
 
 export default function Episode({ episode }: EpisodeProps) {
     return (
-        <h1>{episode.title}</h1>
+        <div className={styles.episode}>
+            <div className={styles.thumbnailContainer}>
+                <Link href='/'>
+                    <button type='button'>
+                        <img src="/arrow-left.svg" alt="Voltar" />
+                    </button>
+                </Link>
+                <Image
+                    width={700}
+                    height={160}
+                    src={episode.thumbnail}
+                    objectFit='cover'
+                />
+                <button type='button'>
+                    <img src="/play.svg" alt="Tocar episódio" />
+                </button>
+            </div>
+
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+            </header>
+
+            <div className={styles.description}
+                dangerouslySetInnerHTML={{ __html: episode.description }} />
+        </div>
     )
 }
 
@@ -56,6 +87,6 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
         props: {
             episode,
         },
-        revalidate : 60 * 60 *24,
+        revalidate: 60 * 60 * 24,
     }
 }
