1b79abcdeac896d096552877da6e4dbe51e95345
Juan Rafael <juanrafaelxpo@outlook.com>
2021-06-11 09:30:04 -0300

    Importação dos audios e criação do play/pause

--- src/contexts/PlayerContexts.ts
+++ src/contexts/PlayerContexts.ts
@@ -11,7 +11,10 @@ type Episode = {
 type PlayerContextData = {
     episodeList: Episode[];
     currentEpisodeIndex: number;
-    play: (episode: Episode) => void
+    isPlaying: boolean;
+    play: (episode: Episode) => void;
+    setIsPlayingState: (state: boolean) => void;
+    togglePlay: () => void;
 }
 
 export const PlayerContext = createContext({} as PlayerContextData);
